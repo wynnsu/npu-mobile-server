bc23c944ef90de4b0a95ee24174ccef78b0d4ee6
Yong <wynnsu@gmail.com>
2017-02-10 07:13:43 -0800

    Add mock data for academic event

---
+++ src/main/resources/AddDataDb.sql
@@ -0,0 +1,8 @@
+/*Academic Event*/
+INSERT INTO `npumobiledb`.`academic_event` 
+(`event_date`, `content`)
+VALUES
+('2017-02-20','Mid-term exams'),
+('2017-02-27','Deadline for change program, graduation petition for 2017 summer semester (without late fee)'),
+('2017-03-10','2017 Summer semester application deadline for international students'),
+('2017-03-13','2017 Summer class schedule and registration packages ready');
